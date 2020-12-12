cbe3335fa7dba307030a2028db135df8528b1d4d
Khavanova Olga <khavanova@mail.ru>
2020-12-12 10:31:08 +0500

    Изменение 6. Создание класса Base и его потомка Desc (BaseAndDesc.h) 2.0 (фиксация всех изменений)

--- Laba5_OOP/Laba5_OOP/BaseAndDesc.h
+++ Laba5_OOP/Laba5_OOP/BaseAndDesc.h
@@ -1,1 +1,38 @@
 #pragma once
+#include <iostream>
+
+using namespace std;
+
+//Реализация класса Base и его потомка Desc
+
+class Base {
+public:
+	Base() {
+		cout << "Конструктор Base()\n";
+	}
+	Base(Base* obj) {
+		cout << "Конструктор Base(Base* obj)\n";
+	}
+	Base(Base& obj) {
+		cout << "Конструктор Base(Base& obj)\n";
+	}
+	~Base() {
+		cout << "Конструктор Base()\n";
+	}
+};
+
+class Desc : public Base {
+public:
+	Desc() {
+		cout << "Конструктор Desc()\n";
+	}
+	Desc(Desc* obj) {
+		cout << "Конструктор Desc(Desc* obj)\n";
+	}
+	Desc(Desc& obj) {
+		cout << "Конструктор Desc(Desc& obj)\n";
+	}
+	~Desc() {
+		cout << "Конструктор Desc()\n";
+	}
+};
